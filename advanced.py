749ad8a6542ed7548528b20163ce4328d7876a0e
ezybear <ezrajooahnlee@gmail.com>
2026-07-13 12:27:48 +0900

    Add advanced.py with example code and comments

---
+++ advanced.py
@@ -0,0 +1,54 @@
+from minecraft import *
+import time
+
+# time.sleep(number)
+# Stops the progam for the given number of seconds. 
+# It is not used very often in real progams. 
+
+# print('1')
+# time.sleep(1)   # Print 1, then wait for 1 second. 
+# print('2')
+# time.sleep(2)   # Print 2, then wait for 2 seconds. 
+# print('3')
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# print('text' or variable or number)
+# The print () function prints the 'text', number, or value of
+# the variable inside the bracket and then adds a newline. 
+# To print multiple items, use a comma (,)
+# If you use print() with nothing inside, it prints an empty line
+
+# name = 'Python'
+# age = 28
+# height = 188
+# print(name)
+# print()         # prints an empty line
+# print(age)
+# print(height)
+# print('hi')
+# print()
+# print(5)
+# print(5*10)
+# print(name, age, height, "hello")
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# input('text' or variabe)
+# It shows text and waits for user to type something and press Enter. 
+# The text or variable can be left out.
+# variable = input('text')
+# This is the most common way to use input(). 
+# If you only use input(), the entered value is not saved. 
+# input() always saves the value as a str type. 
+
+# name = input()
+# print(name)
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+

b1257fcd6d9760c2bd42f14b91c45a918510e657
ezybear <ezrajooahnlee@gmail.com>
2026-07-16 13:15:45 +0900

    Refactor house construction with dynamic positioning
    Refactor house building code to use dynamic position retrieval and improve block definitions.

--- house.py
+++ house.py
@@ -1,25 +1,55 @@
 from minecraft import *
 
-x = 10
-y = 99
-z = 140
+pos = getpos()
+x = pos.x
+y = pos.y
+z = pos.z
 
-setblocks(x, y, z, x-9, y, z, 155)
-setblocks(x, y, z, x, y, z-9, 155)
-setblocks(x-9, y, z-9, x-9, y, z, 155)
-setblocks(x-9, y, z-9, x, y, z-9, 155)
-setblocks(x-1, y, z-5, x-8, y, z-8, 2)
-setblock(x-6, y, z-6, 5,2)
-setblocks(x, y, z-4, x-9, y, z-4, 155)
-setblocks(x-1, y, z-3, x-8, y, z-1, 9)
-setblocks(x-6, y+1, z-5, x-2, y+2, z-5, 5,3)
-setblocks(x-2, y+1, z-5, x-2, y+2, z-8, 5,3)
-setblocks(x-2, y+1, z-8, x-6, y+2, z-8, 5,3)
-setblocks(x-3, y+2, z-8, x-5, y+2, z-8, 20)
-setblocks(x-6, y+1, z-7, x-6, y+2, z-7, 5, 3)
+# Used blocks
+air = 0
+grass = 2
+plank = (5, 2)
+planks = (5, 3)
+water = 9
+glass = 20
+quartz = 155
 
-setblocks(x-1, y+1, z-8, x-1, y+3, z-8, 155)
-setblocks(x-7, y+1, z-8, x-7, y+3, z-8, 155)
-setblocks(x-1, y+3, z-8, x-7, y+3, z-4, 155)
 
-setblocks(x-6, y+3, z-5, x-3, y+3, z-7, 0)
+# Floor/Layout
+setblocks(x, y, z, x-9, y, z, quartz)
+setblocks(x, y, z, x, y, z-9, quartz)
+setblocks(x-9, y, z-9, x-9, y, z, quartz)
+setblocks(x-9, y, z-9, x, y, z-9, quartz)
+setblocks(x-1, y, z-5, x-8, y, z-8, grass)
+setblock(x-6, y, z-6, plank)
+setblocks(x, y, z-4, x-9, y, z-4, quartz)
+
+#Pool
+setblocks(x-1, y, z-3, x-8, y, z-1, water)
+
+#Outline of house
+setblocks(x-6, y+1, z-5, x-2, y+2, z-5, planks)
+setblocks(x-2, y+1, z-5, x-2, y+2, z-8, planks)
+setblocks(x-2, y+1, z-8, x-6, y+2, z-8, planks)
+
+#First floor
+setblocks(x-3, y+2, z-8, x-5, y+2, z-8, glass)
+setblocks(x-6, y+1, z-7, x-6, y+2, z-7, planks)
+
+#Outer detail 1
+setblocks(x-1, y+1, z-8, x-1, y+3, z-8, quartz)
+setblocks(x-7, y+1, z-8, x-7, y+3, z-8, quartz)
+setblocks(x-1, y+3, z-8, x-7, y+3, z-4, quartz)
+
+# Second floor
+setblocks(x-6, y+3, z-5, x-3, y+3, z-7, air)
+setblocks(x-1, y+4, z-4, x-7, y+4, z-4, quartz)
+setblocks(x-2, y+4, z-5, x-2, y+4, z-8, glass)
+setblocks(x-2, y+4, z-8, x-5, y+4, z-8, glass)
+setblocks(x-6, y+3, z-7, x-6, y+3, z-5, planks)
+setblock(x-6, y+4, z-8, planks)
+setblocks(x-6, y+4, z-5, x-6, y+4, z-7, glass)
+
+# Ceiling/Skylight
+setblocks(x-1, y+5, z-4, x-7, y+5, z-8, quartz)
+setblocks(x-3, y+5, z-7, x-5, y+5, z-5, glass)
